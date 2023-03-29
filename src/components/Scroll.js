297efee7cc5ffff6cff9a43377c63dc4fcc87802
slpkn191 <keaw191@live.com>
2023-03-29 16:45:49 +0700

    third commit

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
     return(
-    <div style={{overflowY: 'scroll',border: '1px solid black', height: '800px'}} >
+    <div class = "pa3 ma3" style={{overflowY: 'scroll',border: '1px solid black', height: '800px'}} >
         {props.children}
     </div>
     );
